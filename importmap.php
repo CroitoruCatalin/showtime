a79b8c2e257997af20d5f038b811a74e62e9a0ec
Croitoru Catalin <catalin.croitoru714@gmail.com>
2025-07-01 16:56:29 +0300

    working daisyui

--- importmap.php
+++ importmap.php
@@ -16,13 +16,23 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
     '@hotwired/turbo' => [
-        'version' => '7.3.0',
+        'version' => '8.0.13',
+    ],
+    'daisyui' => [
+        'version' => '5.0.43',
+    ],
+    'daisyui/daisyui.min.css' => [
+        'version' => '5.0.43',
+        'type' => 'css',
+    ],
+    'cally' => [
+        'version' => '0.8.0',
     ],
 ];
