8cfb8c752bd8e932dc63871d64577152247a7d90
Croitoru Catalin <catalin.croitoru714@gmail.com>
2025-07-02 13:21:43 +0300

    rough UI implementation

--- src/Controller/BandController.php
+++ src/Controller/BandController.php
@@ -71,7 +71,7 @@ final class BandController extends AbstractController
     #[Route('/{id}', name: 'app_band_delete', methods: ['POST'])]
     public function delete(Request $request, Band $band, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $band->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $band->getId(), $request->request->get('_token'))) {
             $entityManager->remove($band);
             $entityManager->flush();
         }
